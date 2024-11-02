77e801c000f4b16a9c8fe7029ea4d3d2a336ced0
andrewgavrick <andrewgavrick@yandex.ru>
2024-11-02 14:10:26 +0300

    add result object to data parser

--- lib/tochka_cyclops_api/data_processor.rb
+++ lib/tochka_cyclops_api/data_processor.rb
@@ -6,35 +6,70 @@ require 'securerandom'
 require 'json'
 
 require_relative 'request'
+require_relative 'result'
 require_relative 'schemas/requests/echo'
 
 module TochkaCyclopsApi
   # Module for input data validation and subsequent request invocation
   module DataProcessor
-    def send_request(method, data)
+    def send_request(method:, data:, layer: prod)
+      @uri = uri_for(layer)
       @method = method
       @data = data
       @id = SecureRandom.uuid
+      @schema = shape
+      @validation = @schema&.call(@data.deep_symbolize_keys)
+      @errors = {}
 
-      return @errors.map { |k, v| [k, v].join(' ') }.join(', ') unless valid_params?
-
-      send_data
+      call
     end
 
     private
 
-    def valid_params?
-      if shape
-        result = shape.call(@data.deep_symbolize_keys)
-        @errors = result.errors.to_h
+    def call
+      @errors[:uri] = "Layer you called for is not exist" if @uri.nil?
+      @errors[:schema] = "Schema for method #{@method} is not found" if @schema.nil?
+
+      if @schema
+        @errors[:validation] = validation.errors.to_h
       end
 
-      @errors.empty?
+      if @errors.keys.any?
+        return Result.new(result: nil, errors: @errors)
+      end
+
+      send_data
     end
 
     def send_data
-      TochkaCyclopsApi::Request.send_request(body, @method)
+      TochkaCyclopsApi::Request.send_request(method: @method, body: body)
     end
+
+    def uri_for(layer)
+      {
+        stage: 'https://stage.tochka.com/api/v1/cyclops',
+        pre: 'https://pre.tochka.com/api/v1/cyclops',
+        prod: 'https://api.tochka.com/api/v1/cyclops'
+      }.fetch(layer, nil)
+    end
+
+
+
+    def validate_params(schema)
+      schema.call(@data.deep_symbolize_keys)
+    end
+
+    # def valid_params?(schema)
+    #   if shape
+    #     result = shape.call(@data.deep_symbolize_keys)
+    #     @error = Error.new(
+    #       'Invalid schema',
+    #       result.errors.to_h
+    #     )
+    #   end
+
+    #   @error.description.nil?
+    # end
 
     def shape
       require_relative "schemas/requests/#{@method}"
@@ -42,8 +77,7 @@ module TochkaCyclopsApi
 
       schema.new
     rescue => e
-      @errors = { error: e.message }
-      false
+      nil
     end
 
     def camel_case_method
--- lib/tochka_cyclops_api/result.rb
+++ lib/tochka_cyclops_api/result.rb
@@ -1,6 +1,51 @@
+# frozen_string_literal: true
+
+# откуда сюда можно попасть
+# data_processor
+#   невалидые данные
+#   нет такой схемы
+# request
+#   400 ошибка
+#   500 ошибка
+# response
+#   result
+#   error
+
 # {
-#   result: 'success','error'
-#   data: {
+#   result: 'Success' ('InternalError', 'ApiError')
+#   result_data: {
 #     ...
 #   }
 # }
+
+module TochkaCyclopsApi
+  # Displays the result of request
+  class Result
+    attr_reader :success, :data, :error_message
+
+    def initialize(success, data = nil, error_message = nil)
+      @success = success
+      @data = data
+      @error_message = error_message
+
+      display_result
+    end
+
+    def self.success(data)
+      Result.new(true, data)
+    end
+
+    def self.failure(error_message)
+      Result.new(false, nil, error_message)
+    end
+
+    private
+
+    def display_result
+          {
+            success: @success,
+            data: @data || @error_message
+          }
+        end
+  end
+end
